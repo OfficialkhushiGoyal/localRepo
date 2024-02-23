ec1fb2991a11fe2afa116f48a8fa49cf4ff2087d
Officialkhushigoyal <khushigoyal077696@gmail.com>
2024-02-23 12:23:15 +0530

    add operato

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("hello world");
+
+//variables
+var a = 10;
+var b = 30;
+var c = a + b;
+console.log(c);
+
+
+//data types
+var x = 3;
+var y = "khushi";
+var z = 3.55;
+const p = true;
+var q = undefined;
+var r = null;
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+//object in js 
+let k = {
+  name: "goyal",
+  "job vale" : 5600
+};
+
+console.log(k);
+
+
+//change in object 
+k.salary = "100 lakh"
+console.log(k); 
